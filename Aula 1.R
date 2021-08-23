117e6477b7ca9d0ca5a6a37d06112288482961e0
Thaynara Seminatti <seminattit@gmail.com>
2021-08-23 10:49:06 -0300

    terceira modificação, pacotes e operadores

--- Aula 1.R
+++ Aula 1.R
@@ -45,7 +45,7 @@ JeL
 #c() é uma funcao
 ?c()
 
-#funcoes---
+#Funcoes----
 
 a<-c(10,20,30,40)
 
@@ -58,3 +58,70 @@ summary(JeL)
 
 #1 instalar o pacote caso nao tenha com install.packeges("nome do pacote")
 #2 chamar o pacote library("nome do pacote")
+
+install.packages('stringr')
+library(stringr)
+
+?str_c
+
+nome<-"Thaynara"
+sobrenome<-"Seminatti"
+str_c(nome,sobrenome)
+
+str_c(nome," ",sobrenome)
+
+#Operadores----
+#soma
+b+c
+10+20
+
+#subtracao
+b-d
+10-20
+
+#divisao
+c/b
+10/10
+
+#multiplicacao
+d*b
+10*60
+
+#potencia
+b**2
+b^2
+2^2
+2**2
+
+#identico
+b==c
+b==d
+
+
+#diferente
+b!=c
+d!=c
+
+#maior
+10>50
+50>10
+50>=50
+
+#menor
+10<50
+50<10
+10<=50
+
+#ou
+10>50 | 7>12
+10<50 | 7>12
+
+#e
+10>50 & 7>12
+10<50 | 7<12
+
+#negação
+10<50
+!10<50
+
+
