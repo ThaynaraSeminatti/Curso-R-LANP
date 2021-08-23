a8ac3ce632e19b979790ec17cf6b1e051820e295
Thaynara Seminatti <seminattit@gmail.com>
2021-08-23 09:28:55 -0300

    primeira edição variaveis e operacoes

---
+++ Aula 1.R
@@ -0,0 +1,46 @@
+#curso R
+
+#Introducao----
+
+#deixar o ambiente simpatico
+
+#tools/global options/pane layout
+
+#tools/global options/Apperence
+
+#escolher diretorio Ctrl+Shit+H ou Session/Set Working Directory / Chose Directory
+
+#Criando variaveis----
+#clique em Ctrl+Enter para rodar as linhas
+
+a<-15
+a
+
+b=10
+b
+
+c->10 #da errado
+10->c
+
+#vamos usar sempre a '<-' para evitar problemas
+
+#Operações----
+
+10+15-10
+
+a+b-c
+
+d<-a+b-c
+
+#somando caracter
+
+J<-"joao"
+L<-"luiza"
+
+JeL<-J+L
+JeL
+
+JeL<-c(J,L)
+JeL
+#c() é uma função
+?c()
